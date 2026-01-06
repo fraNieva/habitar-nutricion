de1926feece1c381bb206e32712f70ba44bb8759
Franco Nieva <franconieva86@gmail.com>
2026-01-06 21:34:18 +0100

    fixed language switcher

--- src/components/language-switcher.tsx
+++ src/components/language-switcher.tsx
@@ -9,10 +9,11 @@ import {
 } from "@/components/ui/dropdown-menu";
 import { usePathname, useRouter } from "@/i18n/routing"; // Importamos de TU archivo routing
 import { Languages } from "lucide-react";
-import { useLocale } from "next-intl";
+import { useLocale, useTranslations } from "next-intl";
 
 export function LanguageSwitcher() {
   const locale = useLocale();
+  const t = useTranslations("Navbar");
   const router = useRouter();
   const pathname = usePathname();
 
@@ -25,19 +26,27 @@ export function LanguageSwitcher() {
       <DropdownMenuTrigger asChild>
         <Button variant="ghost" size="icon" className="rounded-full">
           <Languages className="h-[1.2rem] w-[1.2rem]" />
-          <span className="sr-only">Cambiar idioma</span>
+          <span className="sr-only">{t("changeLanguage")}</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent align="end" className="rounded-lg space-y-1">
         <DropdownMenuItem
           onClick={() => handleLocaleChange("es")}
-          className={locale === "es" ? "bg-accent font-bold" : ""}
+          className={
+            locale === "es"
+              ? "bg-primary text-primary-foreground font-bold rounded-md cursor-pointer"
+              : "rounded-md hover:bg-accent cursor-pointer"
+          }
         >
           Español
         </DropdownMenuItem>
         <DropdownMenuItem
           onClick={() => handleLocaleChange("en")}
-          className={locale === "en" ? "bg-accent font-bold" : ""}
+          className={
+            locale === "en"
+              ? "bg-primary text-primary-foreground font-bold rounded-md cursor-pointer"
+              : "rounded-md hover:bg-accent cursor-pointer"
+          }
         >
           English
         </DropdownMenuItem>
